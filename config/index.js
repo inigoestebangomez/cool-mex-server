0f1afbbb28b3f60b9e94f210bd533626ed448504
Iñigo Esteban <nack_z4@hotmail.com>
2024-09-26 13:57:36 +0200

    arreglo de CORS

--- config/index.js
+++ config/index.js
@@ -22,13 +22,9 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors(
-      // credentials: true,
-      // origin: [FRONTEND_URL]
-      cors()
-    )
-  );
+  app.use(cors({
+    origin: 'https://cool-mex.netlify.app'
+}));
 
   // In development environment the app logs
   app.use(logger("dev"));
